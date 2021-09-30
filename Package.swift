18386080aa05922dc4279ece78e0ac1d413e1bcf
prongbang <prongbang@gmail.com>
2021-09-30 20:05:50 +0700

    Update min version ios

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PinCodeField",
     platforms: [
         .macOS(.v11),
-        .iOS(.v15)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
